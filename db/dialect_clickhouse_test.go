9cb0984d04a68db4e531fba64c3d29c9e4e77fc3
Frederik Schöll <fschoell@posteo.de>
2024-11-05 15:17:52 +0100

    add test for invalid date

--- db/dialect_clickhouse_test.go
+++ db/dialect_clickhouse_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
@@ -13,32 +14,46 @@ func Test_convertToType(t *testing.T) {
 		name      string
 		value     string
 		expect    any
+		expectErr error
 		valueType reflect.Type
 	}{
 		{
 			name:      "Date",
 			value:     "2021-01-01",
 			expect:    "2021-01-01",
+			expectErr: nil,
+			valueType: reflect.TypeOf(time.Time{}),
+		}, {
+			name:      "Invalid Date",
+			value:     "2021-99-01",
+			expect:    nil,
+			expectErr: errors.New(`could not convert 2021-99-01 to date: parsing time "2021-99-01": month out of range`),
 			valueType: reflect.TypeOf(time.Time{}),
 		},
 		{
 			name:      "ISO 8601 datetime",
 			value:     "2021-01-01T00:00:00Z",
 			expect:    int64(1609459200),
+			expectErr: nil,
 			valueType: reflect.TypeOf(time.Time{}),
 		},
 		{
 			name:      "common datetime",
 			value:     "2021-01-01 00:00:00",
 			expect:    int64(1609459200),
+			expectErr: nil,
 			valueType: reflect.TypeOf(time.Time{}),
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			res, err := convertToType(test.value, test.valueType)
-			assert.NoError(t, err)
-			assert.Equal(t, test.expect, res)
+			if test.expectErr != nil {
+				assert.EqualError(t, err, test.expectErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, test.expect, res)
+			}
 		})
 	}
 }
